4ebf6a9962e6c7c430078f9b3c4bd5fda21c9d34
Daniel Tse <daniel.tse@sectigo.com>
2023-06-14 14:53:36 -0400

    Add sentiment analysis

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import transformers import pipeline
 
 x = st.slider('Select a value')
 st.write(x, 'squared is', x * x)
+
+pipe = pipeline('sentiment-analysis')
+text = st.text_area('enter some text')
+
+if text:
+    out = pipe(text)
+    st.json(out)
