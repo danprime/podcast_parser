f8474ce4a3d9751f7f77acb8ffbddf9a3503c1cc
Daniel Tse <daniel.tse@sectigo.com>
2023-06-14 15:01:47 -0400

    Change to use Facebook Summarizer

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from transformers import pipeline
 x = st.slider('Select a value')
 st.write(x, 'squared is', x * x)
 
-pipe = pipeline('sentiment-analysis')
+pipe = pipeline("summarization", model='facebook/bart-large-cnn')
 text = st.text_area('enter some text')
 
 if text:
-    out = pipe(text)
+    out = pipe(text, max_length=130, min_length=30, do_sample=False)
     st.json(out)
