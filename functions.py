a88c32a803ea0351a7fd4046cb1b365c8325f122
Daniel Tse <daniel.tse@sectigo.com>
2023-06-15 17:22:59 -0400

    Fix typo

--- functions.py
+++ functions.py
@@ -9,8 +9,9 @@ def transcribe_audio(audiofile):
     print(f"audio_file_session_state:{st.session_state['audio'] }")
 
     #get size of audio file
-    audio_size = round(os.path.getsize(st.session_state['audio'] }")
+    audio_size = round(os.path.getsize(st.session_state['audio'])/(1024*1024),1)
 
     print(f"audio file size:{audio_size}")
+
 
     return audio_size
