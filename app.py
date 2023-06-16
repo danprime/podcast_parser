253b4e47202c2fa7e08dcae1a02812fd730d566b
Daniel Tse <daniel.tse@sectigo.com>
2023-06-16 08:28:05 -0400

    Move transcribe function

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 import streamlit as st
 from transformers import pipeline
 
+def transcribe_audio(audiofile):
+
+    st.session_state['audio'] = audiofile
+
+    print(f"audio_file_session_state:{st.session_state['audio'] }")
+
+    #get size of audio file
+    audio_size = round(os.path.getsize(st.session_state['audio'])/(1024*1024),1)
+
+    print(f"audio file size:{audio_size}")
+
+    return audio_size
+
 st.markdown("# Podcast Q&amp;A")
 
 st.markdown(
