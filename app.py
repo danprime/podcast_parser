e7956b2fb7550c8997b45f057ec01286c23a6a6c
Daniel Tse <daniel.tse@sectigo.com>
2023-06-18 19:12:04 -0400

    Implement Summary

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ def transcribe_audio(audiofile):
     st.session_state['audio'] = audiofile
     print(f"audio_file_session_state:{st.session_state['audio'] }")
 
+    st.info("Getting size of file")
     #get size of audio file
     audio_size = round(os.path.getsize(st.session_state['audio'])/(1024*1024),1)
     print(f"audio file size:{audio_size}")
@@ -20,6 +21,7 @@ def transcribe_audio(audiofile):
     podcast_duration = podcast.duration_seconds
     print(f"Audio Duration: {podcast_duration}")
 
+    st.info("Transcribing")
     whisper_model = whisper.load_model("small.en")
     transcription = whisper_model.transcribe(audiofile)
     st.session_state['transcription'] = transcription
@@ -29,15 +31,9 @@ def transcribe_audio(audiofile):
     return transcription
 
 def summarize_podcast(audiotranscription):
-    sum_pipe = pipeline("summarization",model="google/flan-t5-base",clean_up_tokenization_spaces=True)
-    summarized_text = sum_pipe(audiotranscription,
-                               max_length=1000,
-                               min_length=100,
-                               do_sample=False, 
-                               early_stopping=True,
-                              num_beams=4)
-    summarized_text = ' '.join([summ['summary_text'] for summ in summarized_text])
+    summarizer = pipeline("summarization", model="philschmid/flan-t5-base-samsum", device=0)
 
+    summarized_text = summarizer(audiotranscription)
     return summarized_text
     
 
@@ -55,7 +51,19 @@ st.markdown(
 
 st.audio("marketplace-2023-06-14.mp3") 
 if st.button("Process Audio File"):
-    transcribe_audio("marketplace-2023-06-14.mp3")
+    podcast_text = transcribe_audio("marketplace-2023-06-14.mp3")
+    #write text out
+    st.expander("See Transcription"):
+        st.caption(podcast_text)
+    
+    #Summarize Text
+    podcast_summary = summarize_podcast(podcast_text)
+    st.markdown(
+        """
+           ##Summary of Text
+        """
+        )
+    st.text(podcast_summary)
 
 #audio_file = st.file_uploader("Upload audio copy of file", key="upload", type=['.mp3'])
 
