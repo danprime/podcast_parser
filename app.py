b0e027202d7fa77c518ef897e8442aa10f5420d4
Daniel Tse <daniel.tse@sectigo.com>
2023-06-18 23:58:09 -0400

    Add Dereference text for transcription.

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ if st.button("Process Audio File"):
     podcast_text = transcribe_audio("marketplace-2023-06-14.mp3")
     #write text out
     with st.expander("See Transcription"):
-        st.caption(podcast_text)
+        st.caption(podcast_text['text']})
     
     #Summarize Text
     podcast_summary = summarize_podcast(podcast_text)
